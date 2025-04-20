feb048fc5f94c609edf54cb61c1091afa8372ef2
Cooper Stepanian <cstepanian@zagmail.gonzaga.edu>
2025-04-20 11:48:44 -0700

    Initial Commit

---
+++ F1/F1App.swift
@@ -0,0 +1,17 @@
+//
+//  F1App.swift
+//  F1
+//
+//  Created by Cooper Stepanian on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct F1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
